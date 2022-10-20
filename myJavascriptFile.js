efbc0a5aee5818b4f79391e57a4246609a76553e
Kasdein <kasdein@outlook.fr>
2022-10-20 16:26:20 +0100

    Initial commit

---
+++ myJavascriptFile.js
@@ -0,0 +1,27 @@
+let myFavSmashCharacters = ["Pac-Man", "Mr.Game&Watch", "Mega Man", "Hero", "Mii Brawler"]
+
+for (let index = 0; index < myFavSmashCharacters.length; index++) {
+    console.log(myFavSmashCharacters[index])
+}
+
+for (let index = 1; index <= 12; index++){
+    console.log(index ** 2)
+}
+
+let multiArray = [
+                    ["Seamus Aran", 74],
+                    ["Craig Baevid", 12],
+                    ["David Deckem", 52],
+                    ["Daniel Dier", 41],
+                    ["Erica Dier", 57],
+                    ["Ash Ketchup", 46],
+                    ["Christian Ronaldo", 68],
+                    ["Ash Will", 70]
+                ];
+
+let answer = multiArray[0][0]
+
+let answer2 = multiArray[5][1]
+
+console.log(answer,answer2)
+console.log(answer2)
